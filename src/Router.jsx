0df73ed0d49ca654d040241bc2e2cf6dda4d027f
hilmy <haidaraly98@gmail.com>
2025-12-06 18:55:54 +0700

    fix: update router and add new router for view detail character

--- src/Router.jsx
+++ src/Router.jsx
@@ -7,17 +7,19 @@ import Review from "./pages/Review";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 import Card from "./pages/Card";
+import Detail from "./pages/Detail";
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/home" element={<Home />} />
-      <Route path="/card" element={<Card />} />
+      <Route path="/characters" element={<Card />} />
       <Route path="/product" element={<Product />} />
       <Route path="/about" element={<About />} />
       <Route path="/review" element={<Review />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/characters/:id/:slug" element={<Detail />} />
     </Routes>
   );
 }
